e5391a38952c30f7eef2d219eda15419395764ae
GBuczek <gbuczek@fi.unju.edu.ar>
2026-06-22 11:56:24 -0300

    fix: update seed.ts with Prisma 7 adapter

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,11 @@
 import "dotenv/config";
 import { PrismaClient } from "../src/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 import bcrypt from "bcryptjs";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL! }),
+});
 
 async function main() {
   const password = await bcrypt.hash("admin123", 12);
